28f44a2a601ac4b44f82ee24ae5d0f861bd44e0d
Kdaira <kdktmk@gmail.com>
2021-09-15 19:36:52 +0900

    zipがない場合
    ディレクトリの構造わからないので、`file_names = glob.glob(sys.argv[1] + '*/*')`は適宜変えてください。

--- Script/get_sent_from_BCCWJ.py
+++ Script/get_sent_from_BCCWJ.py
@@ -8,25 +8,20 @@ import os
 import glob
 
 
-def extract_sentences(zfile_name):
-    ''' extract sentences from BCCWJ/SUW/*.zip'''
-    # get zip files infomation
-    zip_file = zipfile.ZipFile(zfile_name, 'r')
-    
-    # extract sentences from BCCWJ
-    for file_name in sorted(zip_file.namelist()):
-        sentences = ''
-        count = 0
-        for line in zip_file.open(file_name, 'r'):
-            line = line.split('\t')
-            try:
-                if line[9] == 'B':
-                    sentences += '\n'
-                sentences += line[22]
-            except:
-                continue
-        open(write_directory + file_name.split('/')[-1].split('.')[0] \
-            + '.txt', 'w').write(sentences)
+def extract_sentences(file_name):
+
+    sentences = ''
+    count = 0
+    for line in open(file_name, 'r'):
+        line = line.split('\t')
+        try:
+            if line[9] == 'B':
+                sentences += '\n'
+            sentences += line[22]
+        except:
+            continue
+    open(write_directory + file_name.split('/')[-1].split('.')[0] \
+        + '.txt', 'w').write(sentences)
 
 
 if __name__ == "__main__":
@@ -35,7 +30,7 @@ if __name__ == "__main__":
     if  glob.glob(write_directory) == []: os.makedirs(write_directory)
     
     # get_zip files infomation
-    zfile_names = glob.glob(sys.argv[1] + '*/*.zip')
-    for zfile_name in zfile_names:
-        extract_sentences(zfile_name)
+    file_names = glob.glob(sys.argv[1] + '*/*')
+    for file_name in file_names:
+        extract_sentences(file_name)
 
